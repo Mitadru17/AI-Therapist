9b9ec7f0ffe2f157c8e7b45635871cc491ef8465
Mitadru17 <mitadruroy006@gmail.com>
2025-05-06 23:49:02 +0530

    Simplify deployment with hardcoded port 3000

--- gunicorn.conf.py
+++
@@ -1,9 +0,0 @@
-# Gunicorn configuration file for Render deployment
-import os
-
-# Bind directly to port 3000 for Render deployment
-bind = "0.0.0.0:3000"
-
-workers = 4
-threads = 2
-timeout = 120 
